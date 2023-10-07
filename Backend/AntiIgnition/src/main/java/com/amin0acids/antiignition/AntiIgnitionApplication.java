b69ea9dff983f88da0ed6f216ef4ea8758ef0abb
Liu-Fayi <liufy520@hotmail.com>
2023-10-07 14:03:35 -0400

    spring

---
+++ Backend/AntiIgnition/src/main/java/com/amin0acids/antiignition/AntiIgnitionApplication.java
@@ -0,0 +1,13 @@
+package com.amin0acids.antiignition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiIgnitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiIgnitionApplication.class, args);
+    }
+
+}
---
+++ Backend/AntiIgnition/src/test/java/com/amin0acids/antiignition/AntiIgnitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amin0acids.antiignition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiIgnitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
