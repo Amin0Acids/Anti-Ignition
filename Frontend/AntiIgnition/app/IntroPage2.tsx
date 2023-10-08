90e934174a91e35ddc44ab3b378bca2f5196c89d
pekachoo <jliu6162@gmail.com>
2023-10-08 12:24:54 -0400

    Update IntroPage2.tsx

---
+++ Frontend/AntiIgnition/app/IntroPage2.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+
+const infoScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.question}>get me the fire risk for...</Text>
+            <TextInput style={styles.input} placeholder="enter address / leave blank for current location" />
+            <Button title="Submit" onPress={() => { console.log("SUBMITTED INFORMATION LOL")}} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'orange',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    question:{
+        fontSize: 40,
+        // in the center of the screen top-down wise
+        top: '20%',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontFamily: 'sans-serif-condensed',
+        color: 'white',
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
+
+export default LoginScreen;
+
