4ea7b5754e7379d0d0fe41ec3987f715981e25f8
DeadPineapple <blockboom557@gmail.com>
2023-10-08 09:13:08 -0400

    not-finished intropage

--- Frontend/AntiIgnition/app/IntroPage.tsx
+++ Frontend/AntiIgnition/app/IntroPage.tsx
@@ -1,10 +1,47 @@
 
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, PanResponder } from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 export let longitudes: number[];
 export let latitudes: number[];
 export let riskLevels: number[];
+
+
+interface draggableWindowProps{
+  color?: string;
+  height?: number;
+  dragY?: boolean;
+  limitY?: number;
+  limitX?: number;
+}
+
+
+export function Draggable(props: draggableWindowProps){
+  const [dragging, setDragging] = useState(false); //set dragging variable to false and link a function to change it
+  const [position, setPosition] = useState({x:0, y:0}); //same thing except for position
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+
+    onPanResponderMove: (event, gestureState) => {
+      setDraggingY(gestureState.dy);
+    },
+  
+    
+  });
+
+  function handleDrag(){
+    props.onDrag(draggingY);
+  }
+  return(
+    <View>
+      <View style={{backgroundColor: props.color, height: props.height}}>
+        <Text>Draggable Window</Text>
+      </View>
+    </View>
+  );
+}
+
 
 function IntroPageUI() {
   function fetchCoordinate() {
@@ -52,11 +89,13 @@ function IntroPageUI() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>AntiIgnition</h1>
-      </header>
-    </div>
+    <View style={{flex:1}}>
+      <View style={{flex:1, backgroundColor:"red"}}>
+          <Text style={{fontSize:8}}>AntiIgnition</Text>
+      </View>
+      <View style={{flex:2}}>
+      </View>
+    </View>
   );
 }
 export default IntroPageUI;
